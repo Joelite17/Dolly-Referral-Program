8e710ea6d843e9455c7b65cb60c36c9b1f633d3c
Joelite17 <chinedujosiahjkl@gmail.com>
2022-03-20 10:55:28 -0700

    added secret key

--- Dan_crypto/settings.py
+++ Dan_crypto/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-is)wf@ktx+bmbogau$!akeoim1prj(sxn%32bd_ovv61y&gkdr'
+SECRET_KEY = os.path.join(BASE_DIR, 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
