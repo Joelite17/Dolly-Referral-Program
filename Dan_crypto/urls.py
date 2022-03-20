926b093a7b331fc4bc74e10c4ef4cba0eb00252b
Joelite17 <chinedujosiahjkl@gmail.com>
2022-03-20 04:36:27 -0700

    added static root

--- Dan_crypto/urls.py
+++ Dan_crypto/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
         template_name='account/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='account/password_reset_complete.html'), name='password_reset_complete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
